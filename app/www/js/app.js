3cc114bbe144273fddd76f8cd0147deb538100da
史安琪 <shianqi@imudges.com>
2016-07-28 09:25:56 +0800

    基础页面搭建

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -7,79 +7,101 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+	.run(function($ionicPlatform) {
+		$ionicPlatform.ready(function() {
+			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+			// for form inputs)
+			if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+				cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+			}
+			if (window.StatusBar) {
+				// org.apache.cordova.statusbar required
+				StatusBar.styleDefault();
+			}
+		});
+	})
 
-.config(function($stateProvider, $urlRouterProvider) {
+	.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+		$ionicConfigProvider.platform.ios.tabs.style('standard');
+		$ionicConfigProvider.platform.ios.tabs.position('bottom');
+		$ionicConfigProvider.platform.android.tabs.style('standard');
+		$ionicConfigProvider.platform.android.tabs.position('standard');
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+		$ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+		$ionicConfigProvider.platform.android.navBar.alignTitle('left');
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+		$ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+		$ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
 
-  // Each tab has its own nav history stack:
+		$ionicConfigProvider.platform.ios.views.transition('ios');
+		$ionicConfigProvider.platform.android.views.transition('android');
+		// Ionic uses AngularUI Router which uses the concept of states
+		// Learn more here: https://github.com/angular-ui/ui-router
+		// Set up the various states which the app can be in.
+		// Each state's controller can be found in controllers.js
+		$stateProvider
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
+		// setup an abstract state for the tabs directive
+			.state('tab', {
+				url: '/tab',
+				abstract: true,
+				templateUrl: 'templates/tabs.html'
+			})
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+			// Each tab has its own nav history stack:
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+			.state('tab.dash', {
+				url: '/dash',
+				views: {
+					'tab-dash': {
+						templateUrl: 'templates/tab-dash.html',
+						controller: 'DashCtrl'
+					}
+				}
+			})
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+			.state('tab.dash-newLocation', {
+				url: '/dash/newLocation',
+				views: {
+					'tab-dash': {
+						templateUrl: 'templates/tab-newLocation.html',
+						controller: 'DashCtrl'
+					}
+				}
+			})
 
-});
+			.state('tab.chats', {
+				url: '/chats',
+				views: {
+					'tab-chats': {
+						templateUrl: 'templates/tab-chats.html',
+						controller: 'ChatsCtrl'
+					}
+				}
+			})
+			.state('tab.chat-detail', {
+				url: '/chats/:chatId',
+				views: {
+					'tab-chats': {
+						templateUrl: 'templates/chat-detail.html',
+						controller: 'ChatDetailCtrl'
+					}
+				}
+			})
+
+			.state('tab.account', {
+				url: '/account',
+				views: {
+					'tab-account': {
+						templateUrl: 'templates/tab-account.html',
+						controller: 'AccountCtrl'
+					}
+				}
+			});
+
+		// if none of the above states are matched, use this as the fallback
+		$urlRouterProvider.otherwise('/tab/dash');
+
+	});
--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,28 +1,68 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+	.controller('ChatsCtrl', function($scope, Chats, $state) {
+		// With the new view caching in Ionic, Controllers are only called
+		// when they are recreated or on app start, instead of every page change.
+		// To listen for when this page is active (for example, to refresh data),
+		// listen for the $ionicView.enter event:
+		//
+		//$scope.$on('$ionicView.enter', function(e) {
+		//});
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+		$scope.chats = Chats.all();
+		$scope.remove = function(chat) {
+			Chats.remove(chat);
+		};
+		$scope.getLocation = function () {
+			$state.go('tab.dash');
+		};
+	})
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+	.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+		$scope.chat = Chats.get($stateParams.chatId);
+	})
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+	.controller('DashCtrl', function($scope, $rootScope, $state) {
+		var locationStorage = localStorage.getItem('location');
+		if(!locationStorage){
+			locationStorage = 'A'
+		}
+		$scope.location = {
+			choice : locationStorage
+		};
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+		$scope.radioChange = function (val) {
+			localStorage.setItem('location',val);
+		};
+		/**
+		 * 跳转到获取新坐标
+         */
+		$scope.goNewLocation = function () {
+			$state.go('tab.dash-newLocation');
+		};
+		$scope.locationString = {
+
+		};
+		$scope.locationStringChange = function () {
+			console.log($scope.locationString);
+		};
+		$scope.refurbish = function (){
+			$rootScope.hello = 'success';
+		};
+	})
+
+	.controller('AccountCtrl', function($scope) {
+		var settingsStorage = JSON.parse(localStorage.getItem('settings'));
+		if(!settingsStorage){
+			settingsStorage = {
+				enableMeituan: true,
+				enableBaidu: true,
+				enableElm: true
+			}
+		}
+
+		$scope.settings = settingsStorage;
+		$scope.settingsChange = function () {
+			localStorage.setItem('settings',JSON.stringify($scope.settings));
+		};
+	});
